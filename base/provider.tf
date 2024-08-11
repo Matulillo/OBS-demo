bf31072a2e6a3f98c6b162c828c78c3fa37a5268
Carlos RH <crodriguezhervas@gmail.com>
2024-08-11 12:06:10 +0200

    Added backend S3 support

--- base/provider.tf
+++ base/provider.tf
@@ -1,13 +1,12 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0.0"
-    }
-  }
-}
-
 provider "aws" {
   region                   = "eu-central-1"
   shared_credentials_files = ["C:\\Users\\NET.IBCSYSADMIN4\\Code\\tf\\credentials"]
+}
+
+terraform {
+  backend "s3" {
+    bucket = "obs-demo-tf-state"
+    key    = "base"
+    region = "eu-central-1"
+  }
 }
